10ec37aa8131479c141b58aef9512612e3695260
Ibnu Halim Mustofa <ibnuhalim@pm.me>
2023-02-01 22:27:07 +0700

    remove mobilenetv2 model

--- base_model.py
+++ base_model.py
@@ -1,6 +1,5 @@
 from keras.applications.vgg16 import VGG16
-from keras.applications.mobilenet_v2 import MobileNetV2
-from keras.layers import Dense, Flatten, Dropout, GlobalAveragePooling2D
+from keras.layers import Dense, Flatten, Dropout
 from keras.models import Model
 from load_image import *
 
@@ -22,13 +21,3 @@ def model_VGG16():
 
     # Create new model
     return Model(inputs=vgg16.input, outputs=x)
-
-def model_mobilenetv2():
-    base_model = MobileNetV2(weights=None,
-                             include_top=False,
-                             input_shape=(img_width, img_height, 3))
-    x = base_model.output
-    x = GlobalAveragePooling2D()(x)
-    x = Dense(256, activation='relu')(x)
-    predictions = Dense(len(class_names), activation='softmax')(x)
-    return Model(inputs=base_model.input, outputs=predictions)
