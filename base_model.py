75f1b0b6442790efcdec993152834a606f994bf3
Ibnu Halim Mustofa <ibnuhalim@pm.me>
2023-02-01 22:26:26 +0700

    modify vgg16 model, add mobilenev2 model

--- base_model.py
+++ base_model.py
@@ -1,5 +1,6 @@
 from keras.applications.vgg16 import VGG16
-from keras.layers import Dense, Flatten, Dropout
+from keras.applications.mobilenet_v2 import MobileNetV2
+from keras.layers import Dense, Flatten, Dropout, GlobalAveragePooling2D
 from keras.models import Model
 from load_image import *
 
@@ -14,9 +15,20 @@ def model_VGG16():
     # Build new classifier
     x = vgg16.output
     x = Flatten()(x)
-    x = Dense(128, activation='relu')(x)
-    x = Dropout(rate=0.2)(x)
+    x = Dense(512,
+              activation='relu')(x)
+    x = Dropout(rate=0.3)(x)
     x = Dense(len(class_names), activation='softmax')(x)
 
     # Create new model
     return Model(inputs=vgg16.input, outputs=x)
+
+def model_mobilenetv2():
+    base_model = MobileNetV2(weights=None,
+                             include_top=False,
+                             input_shape=(img_width, img_height, 3))
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(256, activation='relu')(x)
+    predictions = Dense(len(class_names), activation='softmax')(x)
+    return Model(inputs=base_model.input, outputs=predictions)
